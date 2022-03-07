d6f188ac8ff05aaa589d61b7ddc3dc10d44af1f8
pmanthu <pmanthu@pdx.edu>
2022-03-07 13:00:16 -0800

    HextoBin

--- hextobin.c
+++ hextobin.c
@@ -1,67 +1,72 @@
 #include<stdio.h>
+#include<string.h>
 
 void hextobin(char hexvalue[8])
 {
 int i=0;
+char binvalue[32]="";
 	while(hexvalue[i])
 	{
 		switch(hexvalue[i])
 		{
-			case '0' : printf("0000");
+			case '0' : strcat(binvalue,"0000");
 				break;
-			case '1' : printf("0001");
+			case '1' : strcat(binvalue,"0001");
 				break;
-			case '2' : printf("0010");
+			case '2' : strcat(binvalue,"0010");
 				break;
-			case '3' : printf("0011");
+			case '3' : strcat(binvalue,"0011");
 				break;
-			case '4' : printf("0100");
+			case '4' : strcat(binvalue,"0100");
 				break;
-			case '5' : printf("0101");
+			case '5' : strcat(binvalue,"0101");
 				break;
-			case '6' : printf("0110");
+			case '6' : strcat(binvalue,"0110");
 				break;
-			case '7' : printf("0111");
+			case '7' : strcat(binvalue,"0111");
 				break;
-			case '8' : printf("1000");
+			case '8' : strcat(binvalue,"1000");
 				break;
-			case '9' : printf("1001");
+			case '9' : strcat(binvalue,"1001");
 				break;
-			case 'A' : printf("1010");
+			case 'A' : strcat(binvalue,"1010");
 				break;
-			case 'B' : printf("1011");
+			case 'B' : strcat(binvalue,"1011");
 				break;
-			case 'C' : printf("1100");
+			case 'C' : strcat(binvalue,"1100");
 				break;
-			case 'D' : printf("1101");
+			case 'D' : strcat(binvalue,"1101");
 				break;
-			case 'E' : printf("1110");
+			case 'E' : strcat(binvalue,"1110");
 				break;
-			case 'F' : printf("1111");
+			case 'F' : strcat(binvalue,"1111");
 				break;
-			case 'a' : printf("1010");
+			case 'a' : strcat(binvalue,"1010");
 				break;
-			case 'b' : printf("1011");
+			case 'b' : strcat(binvalue,"1011");
 				break;
-			case 'c' : printf("1100");
+			case 'c' : strcat(binvalue,"1100");
 				break;
-			case 'd' : printf("1101");
+			case 'd' : strcat(binvalue,"1101");
 				break;
-			case 'e' : printf("1110");
+			case 'e' : strcat(binvalue,"1110");
 				break;
-			case 'f' : printf("1111");
+			case 'f' : strcat(binvalue,"1111");
 				break;
 			default : printf("\nInvalid Entry, Please Try Again  %c",hexvalue[i]);
 		}
 		i++;
 	}
+	printf("%s",binvalue);
 }
 
 int main()
 {
     char hexvalue[8];
 	
-	scanf("%s",hexvalue);
-	hextobin(hexvalue);
-
+	printf("Enter Hex value:");
+        scanf("%s",hexvalue);
+        printf("Binary value:");
+        hextobin(hexvalue);
+        printf("\n");
 }
