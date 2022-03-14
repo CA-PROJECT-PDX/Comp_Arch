a47a52614b3d3a6407e7fff654e9510ecfac3f42
pmanthu <pmanthu@pdx.edu>
2022-03-14 11:44:35 -0700

    testcases

---
+++ MEMFILE_GENERATION/ASSEMBLY_FILES/trace_b_j_1.s
@@ -0,0 +1,8 @@
+addi t0,t0,10
+bne t0,t1,16
+lui t2,10234
+addi t2,t2,-145
+beq x0, x0, 8
+jal x1,-8
+addi t3, t3, 1
+jr ra
---
+++ MEMFILE_GENERATION/ASSEMBLY_FILES/trace_st_ad.s
@@ -0,0 +1,5 @@
+addi t3,x0,4
+addi t4,x0,-600
+sw t4,100(t3)
+lw t5,100(t3)
+jr ra
