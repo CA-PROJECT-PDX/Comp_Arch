8ead2a380d6ee782076f9c9231773c7a318e0c63
Narendra Srinivas Raghavaraju <nraghav2@rita.cecs.pdx.edu>
2022-03-13 19:56:13 -0700

    Modifying run.sh

--- Run.sh
+++ Run.sh
@@ -10,4 +10,3 @@ filename1=`basename $line|sed 's/.mem$//g'`
 echo"Triggering mem file ${filename1}.mem"
 ./main ip_file=/u/pmanthu/CA_PROJECT/BASE_CODE/Comp_Arch/${filepath}/${filename1}.mem |tee /u/pmanthu/CA_PROJECT/BASE_CODE/Comp_Arch/OUTPUT_FILES/${filename1}.out
 done
-
