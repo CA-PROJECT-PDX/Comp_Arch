de5cab08e045fcc1ac7103fd93eee61acef414a7
pmanthu <pmanthu@pdx.edu>
2022-03-15 14:41:17 -0700

    Final

--- MEMFILE_GENERATION/memfile_gen.sh
+++ MEMFILE_GENERATION/memfile_gen.sh
@@ -3,14 +3,14 @@
 filepath=$1
 . ~/.bashrc
 echo "\nFiles present in given path are"
-ls -ltr /u/pmanthu/CA_PROJECT/BASE_CODE/Comp_Arch/MEMFILE_GENERATION/${filepath}/
+ls -ltr ${filepath}/
 IFS=':'
-for line in `ls /u/pmanthu/CA_PROJECT/BASE_CODE/Comp_Arch/MEMFILE_GENERATION/${filepath}/*s|xargs|sed 's/ /:/g'`
+for line in `ls ${filepath}/*s|xargs|sed 's/ /:/g'`
 do
 filename1=`basename $line|sed 's/.s$//g'`
 echo "\nThe Object file content for ${filename1}.s is"
-rvas -ahld /u/pmanthu/CA_PROJECT/BASE_CODE/Comp_Arch/MEMFILE_GENERATION/${filepath}/${filename1}.s -o /u/pmanthu/CA_PROJECT/BASE_CODE/Comp_Arch/MEMFILE_GENERATION/OBJ_FILES/${filename1}.o
+rvas -ahld -march=rv32i ${filepath}/${filename1}.s -o ${filepath}/../OBJ_FILES/${filename1}.o
 echo "\nThe mem file content for ${filename1}.o is"
-rvobjdump -d /u/pmanthu/CA_PROJECT/BASE_CODE/Comp_Arch/MEMFILE_GENERATION/OBJ_FILES/${filename1}.o|sed '1,7d'|cut -d'	' -f1,2 > /u/pmanthu/CA_PROJECT/BASE_CODE/Comp_Arch/MEMFILE_GENERATION/MEMFILES/${filename1}.mem
-cat  /u/pmanthu/CA_PROJECT/BASE_CODE/Comp_Arch/MEMFILE_GENERATION/MEMFILES/${filename1}.mem
+rvobjdump -d ${filepath}/../OBJ_FILES/${filename1}.o|sed '1,7d'|cut -d'	' -f1,2 > ${filepath}/../MEMFILES/${filename1}.mem
+cat  ${filepath}/../MEMFILES/${filename1}.mem
 done
--- Run.sh
+++ Run.sh
@@ -5,10 +5,10 @@ obj_file=$2
 #echo "\nFiles present in given path are"
 #ls -ltr /u/pmanthu/CA_PROJECT/BASE_CODE/Comp_Arch/${filepath}/
 IFS=':'
-for line in `ls /u/pmanthu/CA_PROJECT/BASE_CODE/Comp_Arch/${filepath}/*mem|xargs|sed 's/ /:/g'`
+for line in `ls ${filepath}/*mem|xargs|sed 's/ /:/g'`
 do
 filename1=`basename $line|sed 's/.mem$//g'`
 echo "Triggering Mem file ${filename1}.mem"
-./${obj_file} ip_file=/u/pmanthu/CA_PROJECT/BASE_CODE/Comp_Arch/${filepath}/${filename1}.mem |tee /u/pmanthu/CA_PROJECT/BASE_CODE/Comp_Arch/OUTPUT_FILES/${filename1}_${obj_file}.out
+./${obj_file} ip_file=${filepath}/${filename1}.mem |tee ${filepath}/../../OUTPUT_FILES/${filename1}_${obj_file}.out
 done
 
